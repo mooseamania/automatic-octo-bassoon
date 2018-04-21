d30fbd24f20c9380f17c065cc45ce58dae409045
Seamus Owens <seamus.owens@gmail.com>
2018-04-21 19:37:16 -0400

    Saturday Update

--- site/script.js
+++ site/script.js
@@ -1,1 +1,14 @@
 console.log("script.js is Connected!")
+
+$(document).ready(function(){
+    $(".left-block").hover(function(){
+        $(this).css("background-color", "#5870d8");
+        }, function(){
+        $(this).css("background-color", "#3a3a3a");
+    });
+        $(".right-block").hover(function(){
+        $(this).css("background-color", "#d85858");
+        }, function(){
+        $(this).css("background-color", "#7a7a7a");
+    });
+});
