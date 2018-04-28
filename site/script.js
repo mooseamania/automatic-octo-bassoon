b766d451e4a58606f7d745d012b2100af6f35a69
Seamus Owens <seamus.owens@gmail.com>
2018-04-28 12:02:05 -0400

    4/28 updates
    added slide to main page

--- site/script.js
+++ site/script.js
@@ -1,14 +1,26 @@
 console.log("script.js is Connected!")
 
 $(document).ready(function(){
+
+// HOVER SCRIPT
+
     $(".left-block").hover(function(){
         $(this).css("background-color", "#5870d8");
         }, function(){
         $(this).css("background-color", "#3a3a3a");
     });
-        $(".right-block").hover(function(){
+    $(".right-block").hover(function(){
         $(this).css("background-color", "#d85858");
         }, function(){
         $(this).css("background-color", "#7a7a7a");
+    });
+
+// SLIDE SCRIPT
+
+    $(".right-block").click(function(){
+        $(".right-panel").slideDown("slow");
+    });
+    $(".left-block").click(function(){
+        $(".left-panel").slideDown("slow");
     });
 });
